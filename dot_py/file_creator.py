39013ea6fe4ad33a2f352ae4f5f984b679a5bc79
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-03 17:08:43 -0500

    update

--- dot_py/file_creator.py
+++ dot_py/file_creator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import touch
 
 def create_files():
@@ -9,12 +10,25 @@ def create_files():
     a = 1
     b = quantity + 1
 
-    if quantity < 10:
+    cwd = os.getcwd()
+    print("Save to..?")
+    print("'" + cwd + "'", end= " ")
+    response = input("[Y/n]: ")
+    if response in('n', 'N', 'no', 'No', 'NO', 'nO'):
+        location = input("Save to: ")
+        os.chdir(location)
+
+    if quantity == 1:
+        touch.touch(file_name + "." + extension)
+        exit(0)
+    elif quantity < 10:
         fill = 1
-    elif quantity < 100:
+    elif quantity >= 10 and quantity < 100:
         fill = 2
     elif quantity >= 100:
         fill = 3
+    else:
+        fill = 1
 
     for i in range(a, b):
         touch.touch(file_name + str(i).zfill(fill) + "." + extension)
