b4559c6d9aa4e3efff8b4efe349db1a439ba0c3a
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-12 09:58:51 -0500

    update

--- ops_date.py
+++ ops_date.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import calendar
 import numpy as np
 from tqdm import tqdm
-import touch
 
 print("""
 ************************************************************************
