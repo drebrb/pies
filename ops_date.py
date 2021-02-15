4b5bd391df69a6bdf664dfd6442fbbe7b3644b58
Andre Barbosa <andre.jbarbosa@pm.me>
2021-02-15 12:02:23 -0500

    update

--- ops_date.py
+++ ops_date.py
@@ -1,26 +1,7 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
-import calendar
 import numpy as np
 from tqdm import tqdm
-
-print("""
-************************************************************************
-""")
-print(calendar.calendar(datetime.now().year))
-print("""
-************************************************************************""")
-
-print("""
-+-----------------------+
-| Start      End        |
-+-----------+-----------+
-| YYYY-MM-DD YYYY-MM-DD |
-+-----------+-----------+
-""")
-
-start, end = map(np.datetime64, input("Enter dates: ").split())
 
 print("""
 +---------+----------------------+
@@ -38,13 +19,11 @@ print()
 for val, day in tqdm(zip(day_input, days), position=2, colour='green', total=len(days)):
     if not val:
         continue
-    first = np.busday_offset(start, 0, roll='forward', weekmask=day)
-    last = np.busday_offset(end, 1, roll='preceding', weekmask=day)
+    first = np.busday_offset(np.datetime64('2022-01-01'), 0, roll='forward', weekmask=day)
+    last = np.busday_offset(np.datetime64('1002022-01-01'), 1, roll='preceding', weekmask=day)
     delta = np.timedelta64(7, 'D')
     arange = np.arange(first, last, delta)
     array = np.array(arange)
 
     for a in tqdm(array, position=0, leave=False, desc=day, colour='blue'):
         pass
-
-print()
